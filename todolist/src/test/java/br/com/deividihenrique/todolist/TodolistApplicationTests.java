af02210faff6b42fe01123bc4a11baef0cbd85f0
deivsbr <deividi_henrique@hotmail.com>
2023-10-15 19:04:03 -0300

    Dockerfile

---
+++ todolist/src/test/java/br/com/deividihenrique/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deividihenrique.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
